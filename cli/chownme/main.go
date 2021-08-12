9967691c29c410b977b6dbb9b908787de0a341ba
Mathieu Lecarme <mlecarme@bearstech.com>
2021-08-12 21:53:36 +0200

    a client.

--- cli/chownme/main.go
+++ cli/chownme/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/athoune/credrpc/client"
+)
+
+func main() {
+	server := os.Getenv("SERVER")
+	if server == "" {
+		server = "/var/run/chownme/socket"
+	}
+
+	if len(os.Args) == 1 {
+		fmt.Printf("%s path\n", os.Args[0])
+		os.Exit(1)
+	}
+	cli := client.New(server)
+
+	_, err := cli.Call([]byte(os.Args[1]))
+	if err != nil {
+		log.Fatal("Call error:", err)
+	}
 }
